817a9140450a907a892a73d44dfb0ed6f5a706b8
tusharwalzade216 <tusharwalzade216@gmail.com>
2015-09-18 23:02:29 +0530

    Create Optimization.hpp

---
+++ Optimization.hpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<cmath>
+#include<stdlib.h>
+using namespace std;
+
+class Optimization {
+public:
+	void getInput();
+	void northwest();
+	void leastcost();
+	void vam();
+	void sort(int a[], int n);
+	void LCM();
+	void display();
+};
