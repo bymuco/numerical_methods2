8864e1de22292dff348e2601a653d01b10f39937
tushar <tusharwalzade216@users.noreply.github.com>
2015-09-30 19:54:15 +0530

    Update ODE.hpp

--- ODE.hpp
+++ ODE.hpp
@@ -1,10 +1,9 @@
-#include <stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<fstream>
+#include <cstdlib>
+#include <cmath>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
-//#include "gnu_i.hpp"
+
 #define PI 3.1415
 
 using namespace std;
@@ -15,11 +14,36 @@ public:
 	void eulerSeries();
 	void taylorSeries2();
 	void taylorSeries3();
+
+	//second order methods
+	void improvedTangent();
+	void eulerCauchy();
+
+	//third order methods
+	void NystromMethod();
+	void HeunMethod();
+	void ClassicalMethod();
+
+	//fourth order methods
 	void RK();
 	void classicalRK();
+
+	//implicit RK methods
 	void imRK2();
 	void imRK4();
+
+	void AdamsBashforth1();
+	void AdamsBashforth2();
+	void AdamsBashforth3();
+	void NystromMethod1();
+	void NystromMethod2();
+	void AdamsMoulton1();
+	void AdamsMoulton2();
+	void AdamsMoulton3();
+
+	int menu();
 	void plot();
+
 	double f(double, double);
 	double f1(double, double);
 	double f2(double, double);
